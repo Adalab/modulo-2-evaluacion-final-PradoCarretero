02c532856f988b5a272844c9ba4cd4317a6ee734
PradoCarretero <p.carreteroayala@gmail.com>
2022-04-07 18:16:59 +0200

    get from api documents

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,34 @@
 'use strict';
+const searchInput = document.querySelector('.js-search__input');
+let cocktailData = '';
+const cocktailDataList = document.querySelector('.js-cocktail__list');
+const searchButton = document.querySelector('.js-search__button');
 
+/* function renderFilteredList(data) {
+  for (const drink of data) {
+    cocktailDataList.innerHTML = `<li><h2>title</h2><img src="" alt=""></li>`;
+  }
+} */
+function filterDataDrinks(data) {
+  console.log(data);
+}
 
+function getFromApi() {
+  const searchValue = searchInput.value;
+  fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      cocktailData = data.drinks;
+      filterDataDrinks(cocktailData);
+    });
+  /*  renderFilteredList(cocktailData); */
+}
+
+function handleFilter(event) {
+  event.preventDefault();
+  getFromApi();
+}
+
+searchButton.addEventListener('click', handleFilter);
