c227e238819fb8ccfec6058c163b26c8c002fe3e
PradoCarretero <p.carreteroayala@gmail.com>
2022-04-08 20:54:42 +0200

    fix dom of storage alone

--- src/js/main.js
+++ src/js/main.js
@@ -6,26 +6,6 @@ const searchButton = document.querySelector('.js-search__button');
 let cocktailData = [];
 let favorites = [];
 
-function handleCardClick(event) {
-  const clickedItemId = event.currentTarget.id;
-  const objetClicked = cocktailData.find((itemDrink) => {
-    return itemDrink.id === clickedItemId;
-  });
-  const favoritesFound = favorites.findIndex((fav) => {
-    return fav.id === clickedItemId;
-  });
-  if (favoritesFound === -1) {
-    console.log('no estoy entre los favoritos');
-    console.log(favorites);
-    favorites.push(objetClicked);
-  } else {
-    favorites.splice(favoritesFound, 1);
-    console.log('soy favorito');
-  }
-  setFavInLocalStorage();
-  renderFilteredList(cocktailData);
-}
-
 function getLocalStorage() {
   const localStorageFavDrinks = localStorage.getItem('favorites');
 
@@ -34,83 +14,49 @@ function getLocalStorage() {
   } else {
     const arrayDrinks = JSON.parse(localStorageFavDrinks);
     favorites = arrayDrinks;
-
-    console.log('favoritos desde get local');
-    console.log(favorites);
-    let htmlFav = '';
-    for (const drink of favorites) {
-      renderFavoritesLocal(drink);
-    }
-    favoriteDataList.innerHTML = htmlFav;
-    listenDislikeButton();
-  }
-}
-
-function isFavorite(data) {
-  const favoriteFound = favorites.find((fav) => {
-    return fav.id === data.id;
-  });
-  if (favoriteFound === undefined) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-function emptyAvatar(data) {
-  for (const drink of data) {
-    if (drink.image === null) {
-      drink.image = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgdRaq2P7x32D8eIIf-LoP0nehibaasT5SRQ&usqp=CAU`;
-      renderFilteredList(data);
-    } else {
-      renderFilteredList(data);
-    }
+    renderFavoritesLocal();
   }
 }
 
 function renderFavoritesLocal() {
-  favClass = 'drink__favorite';
-  htmlFav += `<li class="js-cocktail__card ${favClass}" id="${drink.id}"><h2>${drink.name}</h2><img class="drink_img" src=${drink.image} alt=""><button id="${drink.id} class="dislikebutton js-dislike-button">MAS</button></li>`;
-}
-
-function renderFilteredList(data) {
-  let html = '';
+  let htmlFav = '';
   let favClass = '';
-  for (const drink of data) {
-    const isFav = isFavorite(drink);
-    if (isFav) {
-    } else {
-      favClass = '';
-      html += `<li class="js-cocktail__card ${favClass}" id="${drink.id}"><h2>${drink.name}</h2><img class="drink_img" src=${drink.image} alt=""></li>`;
-    }
+  for (const drink of favorites) {
+    /* li */
+    favClass = 'drink__favorite';
+
+    const favCard = document.createElement('li');
+    favCard.classList.add('js-cocktail__card');
+    favCard.classList.add(favClass);
+    favCard.setAttribute('id', drink.id);
+    favoriteDataList.appendChild(favCard);
+    /* title */
+    const favCardTitle = document.createElement('h2');
+    const textFavCardTitle = document.createTextNode(drink.name);
+    favCardTitle.appendChild(textFavCardTitle);
+    favCard.appendChild(favCardTitle);
+    /* imagen */
+    const favCardImg = document.createElement('img');
+    favCardImg.classList.add('drink_img');
+    favCardImg.src = drink.image;
+    favCardImg.alt = drink.name;
+    favCard.appendChild(favCardImg);
+    /* boton */
+    const favCardBtn = document.createElement('button');
+    favCardBtn.setAttribute('id', drink.id);
+    favCardBtn.classList.add('dislikebutton');
+    favCardBtn.classList.add('js-dislike-button');
+    const textfavCardBtn = document.createTextNode('MAS');
+    favCardBtn.appendChild(textfavCardBtn);
+    favCard.appendChild(favCardBtn);
+    /* htmlFav += `<li class="js-cocktail__card ${favClass}" id="${drink.id}"><h2>${drink.name}</h2><img class="drink_img" src=${drink.image} alt=""><button id="${drink.id} class="dislikebutton js-dislike-button">MAS</button></li>`; */
   }
-  cocktailCardList.innerHTML = html;
-  favoriteDataList.innerHTML = htmlFav;
-  listenCardClick();
+  console.log('en render');
+  const dislikeButton = document.querySelectorAll('.js-cocktail__card');
+  console.log(dislikeButton);
+  /* favoriteDataList.innerHTML = htmlFav; */
+  console.log();
   listenDislikeButton();
-}
-
-function getFromApi() {
-  const searchValue = searchInput.value;
-  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      cocktailData = data.drinks.map((drink) => {
-        const newDrink = {
-          id: drink.idDrink,
-          name: drink.strDrink,
-          image: drink.strDrinkThumb,
-        };
-        return newDrink;
-      });
-      emptyAvatar(cocktailData);
-    });
-}
-
-function setFavInLocalStorage() {
-  const stringFavDrinks = JSON.stringify(favorites);
-  localStorage.setItem('favorites', stringFavDrinks);
 }
 
 function handleDislikeClick(event) {
@@ -127,32 +73,19 @@ function handleDislikeClick(event) {
   if (favoritesFound !== -1) {
     favorites.splice(favoritesFound, 1);
     console.log('soy favorito');
-  }
-  setFavInLocalStorage();
-  renderFavoritesLocal();
-  renderFilteredList(cocktailData);
-}
-
-function handleSearchButton(event) {
-  event.preventDefault();
-  getFromApi();
-}
-
-function listenCardClick() {
-  const cocktailCard = document.querySelectorAll('.js-cocktail__card');
-  for (const drinkItem of cocktailCard) {
-    drinkItem.addEventListener('click', handleCardClick);
+    setFavInLocalStorage();
+    renderFavoritesLocal();
+    /* renderFilteredList(cocktailData); */
   }
 }
 
 function listenDislikeButton() {
-  console.log('escuchando');
   const dislikeButton = document.querySelectorAll('.js-dislike-button');
+  console.log('escuchando');
+  console.log(dislikeButton);
   for (const dislikeItem of dislikeButton) {
     dislikeItem.addEventListener('click', handleDislikeClick);
   }
 }
-
-searchButton.addEventListener('click', handleSearchButton);
 
 getLocalStorage();
