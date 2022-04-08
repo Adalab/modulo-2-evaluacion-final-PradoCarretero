022dea9e4cc968d683ce0f67e52ff9673c99b4c5
PradoCarretero <p.carreteroayala@gmail.com>
2022-04-08 09:31:35 +0200

    create begining filter

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,19 @@
 'use strict';
 const searchInput = document.querySelector('.js-search__input');
-let cocktailData = [];
 const cocktailDataList = document.querySelector('.js-cocktail__list');
 const searchButton = document.querySelector('.js-search__button');
+let cocktailData = [];
+
+function handleListClick(event) {
+  const clickedItemId = event.currentTarget.id;
+  console.log(clickedItemId);
+  console.log('he clickado');
+}
 
 function emptyAvatar(data) {
   for (const drink of data) {
-    if (drink.strDrinkThumb === null) {
-      drink.strDrinkThumb = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgdRaq2P7x32D8eIIf-LoP0nehibaasT5SRQ&usqp=CAU`;
+    if (drink.image === null) {
+      drink.image = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgdRaq2P7x32D8eIIf-LoP0nehibaasT5SRQ&usqp=CAU`;
       renderFilteredList(data);
     } else {
       renderFilteredList(data);
@@ -17,25 +23,32 @@ function emptyAvatar(data) {
 
 function renderFilteredList(data) {
   for (const drink of data) {
-    cocktailDataList.innerHTML += `<li><h2>${drink.strDrink}</h2><img class="drink_img" src=${drink.strDrinkThumb} alt=""></li>`;
+    cocktailDataList.innerHTML += `<li><h2>${drink.name}</h2><img class="drink_img" src=${drink.image} alt=""></li>`;
   }
 }
 
 function getFromApi() {
   const searchValue = searchInput.value;
-  fetch(
-    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`
-  )
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`;
+  fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      cocktailData = data.drinks;
+      cocktailData = data.drinks.map((drink) => {
+        const newDrink = {
+          id: drink.idDrink,
+          name: drink.strDrink,
+          image: drink.strDrinkThumb,
+        };
+        return newDrink;
+      });
       emptyAvatar(cocktailData);
     });
 }
 
-function handleFilter(event) {
+function handleSearchButton(event) {
   event.preventDefault();
   getFromApi();
 }
 
-searchButton.addEventListener('click', handleFilter);
+searchButton.addEventListener('click', handleSearchButton);
+cocktailDataList.addEventListener('click', handleListClick);
