2b8be1253ea54eb09dcd05e095d2ad0f7f539148
PradoCarretero <p.carreteroayala@gmail.com>
2022-04-09 11:32:32 +0200

    order functions

--- src/js/main.js
+++ src/js/main.js
@@ -6,26 +6,6 @@ const searchButton = document.querySelector('.js-search__button');
 let cocktailData = [];
 let favorites = [];
 
-function handleCardClick(event) {
-  const clickedItemId = event.currentTarget.id;
-  const objetClicked = cocktailData.find((itemDrink) => {
-    return itemDrink.id === clickedItemId;
-  });
-  const favoritesFound = favorites.findIndex((fav) => {
-    return fav.id === clickedItemId;
-  });
-  if (favoritesFound === -1) {
-    console.log('no estoy entre los favoritos');
-    console.log(favorites);
-    favorites.push(objetClicked);
-  } else {
-    favorites.splice(favoritesFound, 1);
-    console.log('soy favorito');
-  }
-  setFavInLocalStorage();
-  renderFilteredList(cocktailData);
-}
-
 function getLocalStorage() {
   const localStorageFavDrinks = localStorage.getItem('favorites');
 
@@ -38,28 +18,6 @@ function getLocalStorage() {
     console.log('favoritos desde get local');
     console.log(favorites);
     renderFavoritesLocal();
-  }
-}
-
-function isFavorite(data) {
-  const favoriteFound = favorites.find((fav) => {
-    return fav.id === data.id;
-  });
-  if (favoriteFound === undefined) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-function emptyAvatar(data) {
-  for (const drink of data) {
-    if (drink.image === null) {
-      drink.image = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgdRaq2P7x32D8eIIf-LoP0nehibaasT5SRQ&usqp=CAU`;
-      renderFilteredList(data);
-    } else {
-      renderFilteredList(data);
-    }
   }
 }
 
@@ -98,6 +56,40 @@ function renderFavoritesLocal() {
   listenDislikeButton();
 }
 
+function handleSearchButton(event) {
+  event.preventDefault();
+  getFromApi();
+}
+
+function getFromApi() {
+  const searchValue = searchInput.value;
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      cocktailData = data.drinks.map((drink) => {
+        const newDrink = {
+          id: drink.idDrink,
+          name: drink.strDrink,
+          image: drink.strDrinkThumb,
+        };
+        return newDrink;
+      });
+      emptyAvatar(cocktailData);
+    });
+}
+
+function emptyAvatar(data) {
+  for (const drink of data) {
+    if (drink.image === null) {
+      drink.image = `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgdRaq2P7x32D8eIIf-LoP0nehibaasT5SRQ&usqp=CAU`;
+      renderFilteredList(data);
+    } else {
+      renderFilteredList(data);
+    }
+  }
+}
+
 function renderFilteredList(data) {
   let html = '';
   let htmlFav = '';
@@ -120,22 +112,35 @@ function renderFilteredList(data) {
   listenDislikeButton();
 }
 
-function getFromApi() {
-  const searchValue = searchInput.value;
-  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      cocktailData = data.drinks.map((drink) => {
-        const newDrink = {
-          id: drink.idDrink,
-          name: drink.strDrink,
-          image: drink.strDrinkThumb,
-        };
-        return newDrink;
-      });
-      emptyAvatar(cocktailData);
-    });
+function handleCardClick(event) {
+  const clickedItemId = event.currentTarget.id;
+  const objetClicked = cocktailData.find((itemDrink) => {
+    return itemDrink.id === clickedItemId;
+  });
+  const favoritesFound = favorites.findIndex((fav) => {
+    return fav.id === clickedItemId;
+  });
+  if (favoritesFound === -1) {
+    console.log('no estoy entre los favoritos');
+    console.log(favorites);
+    favorites.push(objetClicked);
+  } else {
+    favorites.splice(favoritesFound, 1);
+    console.log('soy favorito');
+  }
+  setFavInLocalStorage();
+  renderFilteredList(cocktailData);
+}
+
+function isFavorite(data) {
+  const favoriteFound = favorites.find((fav) => {
+    return fav.id === data.id;
+  });
+  if (favoriteFound === undefined) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 function setFavInLocalStorage() {
@@ -161,11 +166,6 @@ function handleDislikeClick(event) {
     renderFavoritesLocal();
   }
   /* renderFilteredList(cocktailData); */
-}
-
-function handleSearchButton(event) {
-  event.preventDefault();
-  getFromApi();
 }
 
 function listenCardClick() {
